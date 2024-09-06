0424077efdff2e6ed722f0d79a31f3385471c74a
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-09-06 21:04:54 +0900

    초기 업로드

---
+++ src/main/java/com/thxforservice/chattingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.thxforservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class chattingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(chattingserviceApplication.class, args);
+	}
+
+}
