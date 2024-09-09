b63214663480bd1a0af206e487a1534a631b52bc
jeongsuri <jeongsuri96@gmail.com>
2024-09-09 19:28:10 +0900

    feat: 기본 컨트롤러 구성완료

--- src/main/java/com/thxforservice/chat/controllers/chatController.java
+++ src/main/java/com/thxforservice/chat/controllers/chatController.java
@@ -2,11 +2,14 @@ package com.thxforservice.chat.controllers;
 
 import com.thxforservice.global.rests.JSONData;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Chat", description = "채팅 API")
@@ -21,10 +24,34 @@ public class chatController {
         return null;
     }
 
-    @Operation(summary = "채팅방 정보", method = "GET")
+    @Operation(summary = "채팅방 조회(메세지)", method = "GET")
     @ApiResponse(responseCode = "200", description = "채팅방 번호(roomNo)로 채팅방 목록 조회")
     @GetMapping("/room/{roomNo}")
     public JSONData getRoomInfo(@PathVariable Long roomNo) {
+        return null;
+    }
+
+    @Operation(summary = "채팅 시작", method = "POST")
+    @ApiResponse(responseCode = "201")
+    @PostMapping("/room")
+    public ResponseEntity registerRoom() {
+        return null;
+    }
+
+    @Operation(summary = "메세지 전송", method = "POST")
+    @ApiResponse(responseCode = "201")
+    @PostMapping("/message")
+    public ResponseEntity registerMessage(){
+        return null;
+    }
+
+
+
+    @Operation(summary = "채팅방 종료", method = "POST")
+    @ApiResponse(responseCode = "201")
+    @Parameter(name="roomNo", required = true, description = "경로변수, 채팅방 No(roomNo)", example = "1004")
+    @PostMapping("/close/{roomNo}")
+    public ResponseEntity closeRoom(@PathVariable("roomNo") Long roomNo){
         return null;
     }
 }
